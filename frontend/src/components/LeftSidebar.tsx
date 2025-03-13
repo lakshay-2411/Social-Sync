435b9a18ef840281da7371e74a9f0bd78c06c1a5
lakshay-2411 <lakshaynandwani06@gmail.com>
2025-03-13 22:20:42 +0530

    Completed Left sidebar

---
+++ frontend/src/components/LeftSidebar.tsx
@@ -0,0 +1,84 @@
+import {
+  Heart,
+  Home,
+  LogOut,
+  MessageCircle,
+  PlusSquare,
+  Search,
+  TrendingUp,
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { toast } from "sonner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const sidebarItems = [
+  { icon: <Home />, text: "Home" },
+  { icon: <Search />, text: "Search" },
+  { icon: <TrendingUp />, text: "Explore" },
+  { icon: <MessageCircle />, text: "Messages" },
+  { icon: <Heart />, text: "Notifications" },
+  { icon: <PlusSquare />, text: "Create" },
+  {
+    icon: (
+      <Avatar className="w-6 h-6">
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    ),
+    text: "Profile",
+  },
+  { icon: <LogOut />, text: "Logout" },
+];
+
+const LeftSideBar = () => {
+  const navigate = useNavigate();
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/api/v1/user/logout", {
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      if (axios.isAxiosError(error) && error.response) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("An unexpected error occurred");
+      }
+    }
+  };
+
+  const sidebarHandler = (textType: string) => {
+    if (textType === "Logout") {
+      logoutHandler();
+    }
+  };
+
+  return (
+    <div className="fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen">
+      <div className="flex flex-col">
+        <h1>LOGO</h1>
+        <div>
+          {sidebarItems.map((item, index) => {
+            return (
+              <div
+                onClick={() => sidebarHandler(item.text)}
+                key={index}
+                className="flex items-center space-x-2 relative hover:bg-gray-100 cursor-pointer rounder-lg p-3 my-3"
+              >
+                {item.icon}
+                <span>{item.text}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSideBar;
