228b65d7544fc014b73d7bab4d93c519569e29b8
lakshay-2411 <lakshaynandwani06@gmail.com>
2025-03-30 14:17:00 +0530

    Fixed cors issues

--- backend/index.ts
+++ backend/index.ts
@@ -7,13 +7,13 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 dotenv.config({});
 
 const PORT: number = parseInt(process.env.PORT || "3000", 10);
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "I'm coming from backend.", success: true });
-});
+const __dirname = path.resolve();
+console.log(__dirname);
 
 // middlewares
 app.use(express.json());
@@ -31,6 +31,11 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
